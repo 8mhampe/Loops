c09f82f40add1a40620ff135247deec716d17b5b
8mhampe <31480929+8mhampe@users.noreply.github.com>
2017-09-19 09:02:35 -0500

    Add files via upload

---
+++ Loops-master/loops.js
@@ -0,0 +1,58 @@
+function loopOne(time){
+
+  while(time > 0){
+    if(time % 5 == 0)
+    console.log("AHHHHH" +time+ "seconds left");
+
+  else{
+    console.log(time+ "seconds left");}
+    time= time - 1;
+}
+console.log("Done Son!")
+
+}
+
+function loopTwo(number,power){
+
+let sum = 0;
+
+for(let p=0; p<=power; p=p+1){
+    sum = sum+ Math.pow(number,p);
+
+    console.log(sum);
+}
+return(sum);
+}
+
+function loopThree(symbol, number){
+    let lasttime="";
+
+    for (let s = 0; s<=number; s++) {
+        lasttime=lasttime+symbol;
+
+        console.log(lasttime);
+    }
+
+
+}
+
+function main(){
+    //Testing each function
+    loopOne(10);
+    loopOne(25);
+
+    let powersOfTwo = loopTwo(2,10);
+    let powersOfFive = loopTwo(5,4);
+    console.log("The sum is "+powersOfTwo);
+    console.log("The sum is "+powersOfFive);
+
+    loopThree("#",6);
+    loopThree("$",20);
+    loopThree("x",10);
+
+    console.log("You will need to scroll up to check all of them.");
+}
+
+//Hit ctrl + Shift + B to run the program.
+//Hit ESC to make the printout messages go away.
+main();
